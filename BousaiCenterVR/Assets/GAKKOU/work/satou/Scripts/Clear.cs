bb3ab7c134fe99eb5a74793b5838703d60b5375a
DA\nwuser <goroumakisimamu810@gmail.com>
2018-11-29 16:05:04 +0900

    シアタールーム追加して死亡判定(バックドラフト以外)とクリアができる それと細かい調整

--- BousaiCenterVR/Assets/GAKKOU/work/satou/Scripts/Clear.cs
+++ BousaiCenterVR/Assets/GAKKOU/work/satou/Scripts/Clear.cs
@@ -12,9 +12,9 @@
         private float sideFlip = -1;
         private float side = -1;
         private float smooth = 270.0f;
-        private float doorOpenAngle = 90f;
+        public float doorOpenAngle = 90f;
         private bool open = false;
-        private float time = 0;
+        public float time = 0;
 
         private Vector3 defaultRotation;
         private Vector3 openRotation;
@@ -46,7 +46,7 @@
                 SteamVR_Fade.Start(Color.white, 2f);
 
                 //ホワイトアウト終了後メインシーンへ
-                if (time > 2)
+                if (time >= 2)
                 {
                     SceneManager.LoadScene("Clear");
                 }
